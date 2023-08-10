c35ed1d024fa3ffa72fa0a825abb7f32b2b423f1
Maruthi2896 <maruthikj4@gmail.com>
2023-08-10 16:52:52 +0530

    password reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PasswordReset from "./components/Password-reset";
+import SignUp from "./components/Sign-Up";
+import Provide from "./context/Provide";
+import Main from "./components/Main";
+import NewPassword from "./components/newPassword";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Provide>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/password-reset" element={<PasswordReset />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/new-password/:email" element={<NewPassword />} />
+          </Routes>
+        </Provide>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h1>This a main page after a login</h1>
+    </div>
+  );
+}
+
+export default Main;
